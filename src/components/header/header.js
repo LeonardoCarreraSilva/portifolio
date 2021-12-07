5b96bd7e3456a4539ccd53f31ea4af0a0551380e
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2021-12-07 13:55:06 -0300

    finalizacao do Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { Container, Image, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {
+  Container,
+  Figure,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 import Logo from "../../image/logo.jpeg";
 export default function Header() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#">
-          <Image src={Logo} roundedCircle className="Logo" />
+          <Figure>
+            <Figure.Image
+              src={Logo}
+              roundedCircle
+              width={50}
+              height={50}
+              alt="171x180"
+            />
+          </Figure>
         </Navbar.Brand>
         <Nav className="float-right">
-          <Nav.Link href="#">Sobre min</Nav.Link>
-          <Nav.Link href="#">A jornada ate aqui</Nav.Link>
-          <NavDropdown href="#" title="Repositorios">
-            <NavDropdown.Item href="#">Front-End</NavDropdown.Item>
-            <NavDropdown.Item href="#">Back-End</NavDropdown.Item>
-            <NavDropdown.Item href="#">Todos</NavDropdown.Item>
+          <Nav.Link href="/sobre">Sobre min</Nav.Link>
+          <Nav.Link href="/jornada">A jornada ate aqui</Nav.Link>
+          <NavDropdown title="Repositorios">
+            <NavDropdown.Item href="/repo-front">Front-End</NavDropdown.Item>
+            <NavDropdown.Item href="/repo-back">Back-End</NavDropdown.Item>
+            <NavDropdown.Item href="/repo">Todos</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Container>
